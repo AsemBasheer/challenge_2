58158c8cb002c74fc055f640bfbe7bfb574e1f4f
AsemBasheer <AsemBasheer@users.noreply.github.com>
2020-11-03 17:28:38 +0200

    oh no 4

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.post('/', function (req, res) {
     let data = []
     data.push(req.body)
     let result = arrayToCSV(data)
-    res.send(result)
+    console.log("result >>>>", result)
+    res.send(`"${result.join('"\n"').replace(/,/g, '","')}"`)
 });
 
 let port = 3000;
@@ -21,6 +22,23 @@ app.listen(port, function () {
 
 function arrayToCSV(data) {
     let csv = data.map(row => Object.values(row));
-    csv.unshift(Object.keys(data[0]));
-    return `"${csv.join('"\n"').replace(/,/g, '","')}"`;
+    let child = csv[0][csv[0].length - 1]
+    csv[0].pop()
+    console.log(">>>rec", child.length)
+    if (Array.isArray(child) && child.length > 0) {
+        for (let i = 0; i < child.length; i++) {
+            let rec = []
+            rec = arrayToCSV([child[i]])
+            rec.shift()
+            if (rec.length !== 0) {
+                for (let i = 0; i < rec.length; i++) {
+                    csv.push(rec[i])
+                }
+            }
+        }
+    }
+    let keys = Object.keys(data[0])
+    keys.pop()
+    csv.unshift(keys);
+    return csv;
 }
