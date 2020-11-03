e5e6b9b942ef30f478ca48c3148da8e201c8f5f0
AsemBasheer <AsemBasheer@users.noreply.github.com>
2020-11-03 14:25:43 +0200

    oh no ?

--- client/app.js
+++ client/app.js
@@ -1,2 +1,37 @@
+// const { json } = require("express")
 
+document.addEventListener('DOMContentLoaded', () => {
+    const json = document.querySelector('#json')
+    const csv = document.querySelector('#csv')
+    const button = document.querySelector('.w3-button')
+    // json.addEventListener('input', () => {
+    //     console.log((json.value))
+    // })
+    button.addEventListener('click', () => {
+        console.log("click", json.value)
+        loadXMLDoc()
+    })
+})
 
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+            if (xmlhttp.status == 200) {
+                console.log(xmlhttp.responseText)
+                document.getElementById("csv").innerHTML = xmlhttp.responseText;
+            }
+            else if (xmlhttp.status == 400) {
+                alert('There was an error 400');
+            }
+            else {
+                alert('something else other than 200 was returned');
+            }
+        }
+    };
+
+    xmlhttp.open("POST", '/');
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send((json.value));
+}
--- server.js
+++ server.js
@@ -1,2 +1,26 @@
+const express = require('express');
+let app = express();
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/./client'))
+app.use(express.json())
 
+app.post('/', function (req, res) {
+    console.log(req.body)
+
+    res.send(">>csv>>")
+});
+
+let port = 3000;
+
+app.listen(port, function () {
+    console.log(`listening on port ${port}`);
+});
+
+function arrayToCSV(data) {
+    csv = Object.values(data)
+    console.log("csv", csv)
+    console.log("keys 0", Object.keys(data[0]))
+    csv.unshift(Object.keys(data[0]));
+    return `"${csv.join('"\n"').replace(/,/g, '","')}"`;
+}
